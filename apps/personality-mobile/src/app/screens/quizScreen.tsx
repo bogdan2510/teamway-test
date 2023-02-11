508a2f3635f80a5c79052e6a6a47e1a67c5fff9c
Bogdan Ioanas <ioanasbogdan@gmail.com>
2023-02-11 19:50:42 +0200

    mock api. update kcontainer comp

--- apps/personality-mobile/src/app/screens/quizScreen.tsx
+++ apps/personality-mobile/src/app/screens/quizScreen.tsx
@@ -1,11 +1,22 @@
-import { Text } from 'react-native';
 import React from 'react';
-import { KContainer } from '@teamway-test/ui-kit';
+import { KContainer, KSpacer, KTitle } from '@teamway-test/ui-kit';
+import { Button, View } from 'react-native-ui-lib';
 
 const QuizScreen = () => {
   return (
-    <KContainer>
-      <Text>QuizScreen</Text>
+    <KContainer center={false}>
+      <View padding-20>
+        <KTitle
+          title="You’re really busy at work and a colleague is telling you their life
+          story and personal woes. You:"
+        />
+        <KSpacer height={20} />
+        <Button
+          outline={true}
+          label="Don’t dare to interrupt them"
+          borderRadius={10}
+        />
+      </View>
     </KContainer>
   );
 };
--- libs/ui-kit/src/lib/kcontainer/KContainer.tsx
+++ libs/ui-kit/src/lib/kcontainer/KContainer.tsx
@@ -3,11 +3,12 @@ import { View, Colors } from 'react-native-ui-lib';
 
 export interface KContainerProps {
   children: React.ReactNode;
+  center?: boolean;
 }
 
 export function KContainer(props: KContainerProps) {
   return (
-    <View flex backgroundColor={Colors.purple70} center>
+    <View flex backgroundColor={Colors.purple70} center={props.center ?? true}>
       {props.children}
     </View>
   );

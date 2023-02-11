7ef2041c54aceaffccfade30475eba7024df21af
Bogdan Ioanas <ioanasbogdan@gmail.com>
2023-02-11 20:36:25 +0200

    added react query, axios

--- apps/personality-mobile/src/app/App.tsx
+++ apps/personality-mobile/src/app/App.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from './navigation';
 
-const App = () => <Navigation />;
+import { focusManager, QueryClient, QueryClientProvider } from 'react-query';
+import { AppState, AppStateStatus, Platform } from 'react-native';
+
+const App = () => {
+  function onAppStateChange(status: AppStateStatus) {
+    if (Platform.OS !== 'web') {
+      focusManager.setFocused(status === 'active');
+    }
+  }
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onAppStateChange);
+
+    return () => subscription.remove();
+  }, []);
+
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navigation />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
--- apps/personality-mobile/src/app/screens/homeScreen.tsx
+++ apps/personality-mobile/src/app/screens/homeScreen.tsx
@@ -1,11 +1,23 @@
 import { RootStackParamProps } from '@teamway-test/shared-types';
 import { KContainer, KSpacer, KSubtitle, KTitle } from '@teamway-test/ui-kit';
+import axios from 'axios';
 import React from 'react';
 import { useWindowDimensions } from 'react-native';
 import { View, Button } from 'react-native-ui-lib';
+import { useQuery } from 'react-query';
+
+const apiUrl = 'https://mocki.io/v1/0ea90288-9bef-481c-9dc4-28464ff0c1a4';
 
 const HomeScreen = ({ navigation }: RootStackParamProps<'HomeScreen'>) => {
   const { height } = useWindowDimensions();
+  const getQuizData = async () => {
+    const { data } = await axios.get(apiUrl);
+    return data;
+  };
+  const { data, isLoading, isError } = useQuery('quizData', getQuizData);
+
+  console.log(data);
+
   return (
     <KContainer>
       <View height={height * 0.2} paddingT-60 paddingH-20>
